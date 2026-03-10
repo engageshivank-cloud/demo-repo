7717508bd1de7a020060a9b2236e78d28c77ad26
Ankit <ankit@hrengage.ai>
2026-03-10 15:43:15 +0500

    basic views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import Blog
+from rest_framework.response import Response
 
-# Create your views here.
+def blog_detail(request, pk):
+    blog = get_object_or_404(Blog, pk=pk)
+    return Response({'id': blog.id, 'name': blog.name, 'content': blog.content})
