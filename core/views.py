586a2335ec8547f924b0385e51f6adbb8b1dda46
Ankit <ankit@hrengage.ai>
2026-03-10 15:47:13 +0500

    post view

--- core/views.py
+++ core/views.py
@@ -4,4 +4,12 @@ from rest_framework.response import Response
 
 def blog_detail(request, pk):
     blog = get_object_or_404(Blog, pk=pk)
-    return Response({'id': blog.id, 'name': blog.name, 'content': blog.content})
+    return Response({'id': blog.id, 'name': blog.name, 'content': blog.content})    
+
+def post_blog(request):
+    if request.method == 'POST':
+        serializer = BlogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
