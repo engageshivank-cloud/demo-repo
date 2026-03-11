4d69217916ad3c13815c2834184d420c5deb63a3
Ankit <ankit@hrengage.ai>
2026-03-11 12:02:09 +0500

    added profile model

--- core/models.py
+++ core/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
 
 class Blog(models.Model):
     name=models.CharField(max_length=100)
