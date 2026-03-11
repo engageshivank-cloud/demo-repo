58d16964463d51e4a22eb616a16712c0886bea5e
Ankit <ankit@hrengage.ai>
2026-03-11 10:47:11 +0500

    added comment

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from .models import Blog
 from rest_framework.response import Response
-from .serializers import BlogSerializer
+from .serializers import BlogSerializer, CommentSerializer
 from rest_framework.views import APIView
 
 class BlogDetail(APIView):
@@ -29,4 +29,36 @@ class BlogDetail(APIView):
     def delete(self, request, pk):
         blog = get_object_or_404(Blog, pk=pk)
         blog.delete()
+        return Response(status=204)
+
+
+class CommentDetail(APIView):
+    def get(self, request, blog_pk, pk):
+        blog = get_object_or_404(Blog, pk=blog_pk)
+        comment = get_object_or_404(blog.comments, pk=pk)
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
+
+    def put(self, request, blog_pk, pk):
+        blog = get_object_or_404(Blog, pk=blog_pk)
+        comment = get_object_or_404(blog.comments, pk=pk)
+        serializer = CommentSerializer(comment, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+    def patch(self, request, blog_pk, pk):
+        blog = get_object_or_404(Blog, pk=blog_pk)
+        comment = get_object_or_404(blog.comments, pk=pk)
+        serializer = CommentSerializer(comment, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+    def delete(self, request, blog_pk, pk):
+        blog = get_object_or_404(Blog, pk=blog_pk)
+        comment = get_object_or_404(blog.comments, pk=pk)
+        comment.delete()
         return Response(status=204)
